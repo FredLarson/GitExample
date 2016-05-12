e5838a951dd733fa03e9b6b892818246345160fc
Kevin Cornwell <kevin@thinkbigfish.com>
2016-05-12 11:21:18 -0700

    test

--- Existing Project/ViewController.swift
+++ Existing Project/ViewController.swift
@@ -13,7 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        // extra comment!
+        // extra comment! one
+        // another comment
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
